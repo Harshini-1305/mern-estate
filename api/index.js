2bcbc04abb401a78dd0eb76b17489d1f8b019b71
Harshini-1305 <harshini_movva@srmap.edu.in>
2025-04-17 23:04:27 +0530

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,11 +26,20 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter); // Use the imported router
 app.use('/api/auth', authRouter); // Use the imported 
-app.get('/test', (req, res) => {
-  res.json(
-    {
-      message:'Hello World!',
+//app.get('/test', (req, res) => {
+  //res.json(
+    //{
+      //message:'Hello World!',
+//});
+//}
+//);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
-}
-);
 
